3e5a320e34af797d5bc5fac017ebd960a7bb8430
Alan Oliveira <alanprot7@gmail.com>
2023-06-11 16:45:39 -0300

    Aulas 16 e 17

--- app/Http/Requests/StoreUpdateUserFormRequest.php
+++ app/Http/Requests/StoreUpdateUserFormRequest.php
@@ -23,12 +23,17 @@ class StoreUpdateUserFormRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        // dd($this->segment(2)); SEGMENT PARA PEGAR AS PARTES DO LINK
+        // dd($this->id); IGUAL SO QUE FUNCIONA COM O NOME DO PARAMETRO
+
+        $id = $this->id ?? ''; // Esta expressão considera o valor de id, se for nulo ele volta vazio
+
+        $rules = [
             'name' => 'required|string|max:255|min:3',
             'email' => [
                 'required',
                 'email',
-                'unique:users',
+                "unique:users,email,{$id},id",
             ],
             'password' => [
                 'required',
@@ -36,5 +41,15 @@ class StoreUpdateUserFormRequest extends FormRequest
                 'max:15',
             ]
         ];
+
+        if($this->method('PUT')){
+            $rules['password'] = [
+                'nullable',
+                'min:6',
+                'max:15',
+            ];
+        }
+
+        return $rules;
     }
 }
